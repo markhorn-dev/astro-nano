3fb4a454851f8915b9ea51b12e2ecac66b3bca4d
jfelding <79907020+jfelding@users.noreply.github.com>
2024-04-06 19:20:45 +0200

    Remove-cloudflare-ssr-integration (#5)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,10 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-nano-demo.vercel.app",
   integrations: [mdx(), sitemap(), tailwind()],
-  output: "server",
-  adapter: cloudflare()
+  output: "static",
 });
