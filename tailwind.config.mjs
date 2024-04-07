f8388308903c9733bc231819a2431d36a25eceef
Jacob Felding <jfelding@gmail.com>
2024-04-07 10:23:52 +0200

    styling of light theme and header

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -12,6 +12,26 @@ export default {
         sans: ["Inter", ...defaultTheme.fontFamily.sans],
         serif: ["Lora", ...defaultTheme.fontFamily.serif],
       },
+
+
+      colors: {
+        olive: {
+          DEFAULT: '#F8FAF2',
+          '50': '#F8FAF2',
+          '100': '#F2F4E6',
+          '200': '#DDE0C1',
+          '300': '#C7CB9B',
+          '400': '#B0B474',
+          '500': '#A2AB8C',
+          '600': '#7E846F',
+          '700': '#5B6052',
+          '800': '#373A35',
+          '900': '#141613',
+        },
+
+
+      },
+
     },
   },
   plugins: [require("@tailwindcss/typography")],
