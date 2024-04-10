217219d213ced40ae1abb1e374bfb363fec4b9b5
jfelding <79907020+jfelding@users.noreply.github.com>
2024-04-10 09:19:24 +0200

    image optimization (#6)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-nano-demo.vercel.app",
-  integrations: [mdx(), sitemap(), tailwind()],
-  output: "static",
+  integrations: [mdx(), sitemap(), tailwind(), astroImageTools],
+
 });
