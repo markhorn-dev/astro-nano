0bf964fc4b1108ae5ce639dcc3d51645a4d0b4a5
Chris De Sousa <cdelasoul@gmail.com>
2024-09-20 12:23:13 -0700

    Fix: add test site url to see if it fixes deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
+  site: "https://test-homepage.codemyriad.io",
   integrations: [mdx(), sitemap(), tailwind()],
 });
