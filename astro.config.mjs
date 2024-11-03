2ca0d0544133755fd8455de1b5c430a5bcd6341b
rjhoppe <rickjhoppe@gmail.com>
2024-11-03 16:15:42 -0500

    feat: Added simple pw auth to gifts page Working on db and gift table part next

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,10 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-nano-demo.vercel.app",
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap(), tailwind(), react()]
 });
