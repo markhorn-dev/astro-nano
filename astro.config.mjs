8629f7be72789e760c9e32e9406368b0b65e9ad9
rjhoppe <rickjhoppe@gmail.com>
2024-11-04 08:39:02 -0500

    feat: Setup drizzle and sqlite db

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,16 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-nano-demo.vercel.app",
-  integrations: [mdx(), sitemap(), tailwind(), react()]
+  integrations: [mdx(), sitemap(), tailwind(), react()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
