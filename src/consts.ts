4b70443b127a002ea2ad1fa7afea2cfe9b753d68
guilhermefgs <guilhermefgs@proton.me>
2024-11-20 23:42:02 -0300

    testing github pages

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
-  NAME: "Astro Nano",
-  EMAIL: "markhorn.dev@gmail.com",
+  NAME: "Guilherme's webcorner",
+  EMAIL: "guilhermefgs@proton.me",
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
