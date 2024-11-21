38e4c95d0a6cfb021dc7dda56a3f6de28d5297fc
guilhermefgs <guilhermefgs@proton.me>
2024-11-21 12:24:27 -0300

    change: site metadata

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
-  NAME: "Guilherme's webcorner",
+  NAME: "Gui's web palace",
   EMAIL: "guilhermefgs@proton.me",
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
@@ -10,7 +10,7 @@ export const SITE: Site = {
 
 export const HOME: Metadata = {
   TITLE: "Home",
-  DESCRIPTION: "Astro Nano is a minimal and lightweight blog and portfolio.",
+  DESCRIPTION: "Guilherme's corner on the web.",
 };
 
 export const BLOG: Metadata = {
