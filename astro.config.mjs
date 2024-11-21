08bf12c6b14317b6fcf871136a54e9e8a8aa62c7
guilhermefgs <guilhermefgs@proton.me>
2024-11-20 23:37:17 -0300

    innitial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
+  site: "https://guilhermefgs.github.io",
   integrations: [mdx(), sitemap(), tailwind()],
 });
