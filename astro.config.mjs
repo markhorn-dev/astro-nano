0cd44971c4bf84181062e7459bb4d2a356e7683f
Nitesh Rijal <rijal.it@gmail.com>
2024-11-24 14:45:09 -0600

    change base url to niteshrijal.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import solidJs from "@astrojs/solid-js"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-sphere-demo.vercel.app",
+  site: "https://niteshrijal.com",
   integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
 })
