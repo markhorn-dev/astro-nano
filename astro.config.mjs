f39f055d260d47f733af035e7977a33e85fc4059
Marek Szkudelski <marek.szkudelski@gmail.com>
2025-01-08 15:02:26 +0100

    remove netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import netlify from '@astrojs/netlify';
+import node from '@astrojs/node';
 
 export default defineConfig({
   site: 'https://szkudelski.dev/',
   output: 'static',
-  adapter: netlify(),
+  adapter: node({
+    mode: 'standalone',
+  }),
   integrations: [mdx(), sitemap(), tailwind()],
 });
