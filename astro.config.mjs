b29a34e6c1082749cdea1e0905028d2d69636911
ahmadmuhammadgd <ahmadmuhammadgd@gmail.com>
2025-02-06 17:22:50 +0200

    init searchbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,11 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import rehypeMermaid from 'rehype-mermaid';
 import addMermaidClass from './add-mermaid-classname';
+import pagefind from "astro-pagefind"
 
 export default defineConfig({
   site: "https://ahmadmuhammadgd.github.io",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), pagefind(),],
   markdown: {
     rehypePlugins: [
       ["rehype-mermaid", { launchOptions: { executablePath: null } }]
